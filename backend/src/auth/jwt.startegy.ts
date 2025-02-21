302fd18d4f2abd267fda46e4b34818ef57f564e9
S.Shreyas <65349480+shreyas-s19@users.noreply.github.com>
2025-02-21 11:51:31 +0530

    Cookie Setting changs

--- backend/src/auth/jwt.startegy.ts
+++ backend/src/auth/jwt.startegy.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) => {
+          let token = null;
+          if (req && req.cookies) {
+            token = req.cookies['access_token'];
+          }
+          return token;
+        },
+      ]),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
     });
